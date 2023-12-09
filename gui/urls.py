15a680b24b2f452df6561d34a4ddd6e0923c6b8d
VirtualMarco <marco.randazzo@wealthype.it>
2023-12-09 18:25:45 +0100

    css ok, js no

--- gui/urls.py
+++ gui/urls.py
@@ -4,6 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('dashboard', views.dashboard, name='home2'),
-    path('shop', views.home3, name='home3'),
-    path('gioco', views.home4, name='home4'),
 ]
