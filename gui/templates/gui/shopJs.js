811dc39111d5fbde8eafe60af4628ab356aa8a10
AngeloRan <arandazzo300990@gmail.com>
2023-12-27 21:11:27 +0100

    progresso shop

--- gui/templates/gui/shopJs.js
+++ gui/templates/gui/shopJs.js
@@ -2,55 +2,54 @@
 
 config.rivelatoreSezioni()
 
+// settare Uppercase lista
 
-// ORDINA PER ORDINE ALFABETICO
+const articoliArr = [{titolo: 'naruto', tipo: 'statuina', numero: 4, img: '../../static/images/Grut.png', inserimento: Date.now() - 500 }, {titolo: 'harry Potter', tipo: 'statuina', numero: 4, img: 'https://i.pinimg.com/564x/ab/51/f2/ab51f2dd6f1bbd2c8c435e4e8d403d93.jpg', inserimento: Date.now() - 1000}, {titolo: ' giangiukan', tipo: 'statuina', numero: 4, img: 'https://i.pinimg.com/564x/ab/51/f2/ab51f2dd6f1bbd2c8c435e4e8d403d93.jpg', inserimento: Date.now() - 2000}, {titolo: 'dragonball', tipo: 'statuina', numero: 4, img: 'https://i.pinimg.com/564x/ab/51/f2/ab51f2dd6f1bbd2c8c435e4e8d403d93.jpg', inserimento: Date.now() - 700}, {titolo: 'ilsignoredeglianelli', tipo: 'statuina', numero: 4, img: 'https://i.pinimg.com/564x/ab/51/f2/ab51f2dd6f1bbd2c8c435e4e8d403d93.jpg', inserimento: Date.now() - 8000}];
 
+const upperLista = function () {
+const categorie = document.querySelectorAll('.categoriaarticolo');
+categorie.forEach(c => c.textContent = c.textContent.trim()[0].toUpperCase() + c.textContent.trim().slice(1));
+const titoli = document.querySelectorAll('.nomearticolo');
+titoli.forEach(c => c.textContent = c.textContent.trim()[0].toUpperCase() + c.textContent.trim().slice(1))}
+upperLista()
+const ALLIN_ART_VETRINA = -50;
+const vetrinaEl = document.querySelector('.vetrina');
+let pulsanti = [...document.querySelectorAll('.btn_vet')];
+const listaEl = document.querySelector('.contenitoreArticoli');
+const headerEl = document.querySelector('header')
+const menuArticoliEl = document.querySelector('.div-menuarticoli')
 
-const articoliArr = [{titolo: 'naruto', tipo: 'statuina', numero: 4, img: 'https://i.pinimg.com/564x/ab/51/f2/ab51f2dd6f1bbd2c8c435e4e8d403d93.jpg' }, {titolo: 'harry Potter', tipo: 'statuina', numero: 4, img: 'https://i.pinimg.com/564x/ab/51/f2/ab51f2dd6f1bbd2c8c435e4e8d403d93.jpg'}, {titolo: 'Giangiukan', tipo: 'statuina', numero: 4, img: 'https://i.pinimg.com/564x/ab/51/f2/ab51f2dd6f1bbd2c8c435e4e8d403d93.jpg'}, {titolo: 'dragonball', tipo: 'statuina', numero: 4, img: 'https://i.pinimg.com/564x/ab/51/f2/ab51f2dd6f1bbd2c8c435e4e8d403d93.jpg'}, {titolo: 'ilsignoredeglianelli', tipo: 'statuina', numero: 4, img: 'https://i.pinimg.com/564x/ab/51/f2/ab51f2dd6f1bbd2c8c435e4e8d403d93.jpg'}];
+console.log(Number.parseFloat(getComputedStyle(document.querySelector('.shopsinistra')).width, 10));
 
+const headerObserver = new IntersectionObserver(function (entries) {
+  const[entry] = entries
+  if (!entry.isIntersecting) {
+    document.getElementById('hamMenuLista').classList.remove('hidden-1');
+    document.getElementById('hamMenuLista').style.width = Number.parseFloat(getComputedStyle(document.querySelector('.shopsinistra')).width, 10)+'px';
+    document.getElementById('hamMenuLista').style.height = '50px'
+   document.getElementById('hamMenuLista').style.position = 'fixed'
+  }
+  else   {
+    document.getElementById('hamMenuLista').classList.add('hidden-1');
+    document.getElementById('hamMenuLista').style.width = ''
+    document.getElementById('hamMenuLista').style.position = ''
+    document.getElementById('hamMenuLista').style.height = ''
+  }}, {
+  root: null,
+  threshold: 0,
+  rootMargin: `${getComputedStyle(menuArticoliEl).height}`
+});
 
-const funzioneSort = function (nonrev = true) {
+headerObserver.observe(headerEl);
 
-  const markup = articoliArr.toSorted((a, b) => {
-
-    if(!nonrev) {
-      if (a.titolo.toUpperCase() > b.titolo.toUpperCase()) return -1;
-      if (a.titolo.toUpperCase() < b.titolo.toUpperCase()) return 1
-    } else{
-      if (a.titolo.toUpperCase() > b.titolo.toUpperCase()) return 1;
-      if (a.titolo.toUpperCase() < b.titolo.toUpperCase()) return -1
-    }}).map(el => `    
-      <div class="articoli container">
-        <figure class="figure-img">
-          <img class="img_art" src=${el.img} alt="">
-        </figure>
-        <div class="articoli_label">
-        <h4 class="nomearticolo">${el.titolo}</h4>
-        <p class="categoriaarticolo">${el.tipo}</p>
-        </div>
-      </div>`).join('')
-
-    document.querySelector('.contenitoreArticoli').innerHTML = '';
-    document.querySelector('.contenitoreArticoli').insertAdjacentHTML('afterbegin', markup)
-}
-
-document.getElementById('sortAl').addEventListener('click', funzioneSort)
-document.getElementById('sortRevAl').addEventListener('click', function() {
-  funzioneSort(false)
+window.addEventListener('resize', function () {
+  document.getElementById('hamMenuLista').style.width = Number.parseFloat(getComputedStyle(document.querySelector('.shopsinistra')).width, 10)+'px';
 })
 
 
 
-const ALLIN_ART_VETRINA = -50;
-const vetrinaEl = document.querySelector('.vetrina');
-const pulsanti = [...document.querySelectorAll('.btn_vet')];
-
-// VETRINA
-
-
-
-const fnVetrina = function () {
-  let curArt = 6;
+const fnVetrina = function (totale) {
+  let curArt = 2;
   let articoliVetrinaArr = [...document.querySelectorAll('.articoloInVetrina')]
   let elCentrale;
   let ultimoClick = 0;
@@ -61,8 +60,8 @@ const fnVetrina = function () {
    el.classList.remove('transitionclass');
    el.style.transform = `translateX(${ALLIN_ART_VETRINA + (100 * (i - curArt))}%) ${ i !== curArt ? 'scale(0.5)' : ''}`;
     });
-  articoliVetrinaArr[6].classList.remove('sfocArtVet');
-  elCentrale = articoliVetrinaArr[6];
+  articoliVetrinaArr[curArt].classList.remove('sfocArtVet');
+  elCentrale = articoliVetrinaArr[curArt];
 
   setTimeout(function () {
     articoliVetrinaArr.forEach(el=> el.classList.add('transitionclass'))
@@ -70,9 +69,9 @@ const fnVetrina = function () {
 
 // FUNZIONE VETRINA LOOP
     const vetLoop = function () {
-      const LIMITE = this === document.querySelector('.next-pg') ? 16 : 5;
+      const LIMITE = this === document.querySelector('.next-pg') ? (totale + 2) : 1;
       const SELETTORE_NEXT_PREV = this === document.querySelector('.next-pg') ? 1 : -1
-      const TOT_ART = this === document.querySelector('.next-pg') ? -10 : 10;
+      const TOT_ART = this === document.querySelector('.next-pg') ? -totale : totale;
       const ora = Date.now()
       if (ora - ultimoClick >= intervalloClick) {
       
@@ -105,14 +104,9 @@ const fnVetrina = function () {
         el.style.transform = `translateX(${ALLIN_ART_VETRINA + (100 * (i - curArt))}%) ${ i !== curArt ? 'scale(0.5)' : ''}`;
         }); ultimoClick = ora;
     }}}
-    
-
- 
-
-
-
-
-
+  
+  
+  
   pulsanti.forEach(el => el.addEventListener('click', vetLoop ))
 
   document.addEventListener('keydown', function (e) {
@@ -140,65 +134,113 @@ const fnVetrina = function () {
     vetrinaAuto = setInterval(vetLoop.bind(document.querySelector('.next-pg')), 6000)
   })
 
-
-
-  
-// INGRANDIMENTO ELEMENTO CORRENTE
-
-    // const arra = ['mousedown','mouseup'];
-
-    //   arra.forEach((el) => 
-    //   {const callback = function(e) {
-    //   if(e.target.parentElement === elCentrale) { 
-    //   String(this) === 'mousedown' ? elCentrale.classList.remove('transitionclass') : elCentrale.classList.add('transitionclass');
-    //   String(this) === 'mousedown' ? elCentrale.classList.add('transitionclassVel') : elCentrale.classList.remove('transitionclassVel');
-    //   elCentrale.style.zIndex = `${String(this) === 'mousedown' ? 1 : ''}`
-    //   // elCentrale.style.transition = `all${String(this) === 'mousedown' ? '0.3' : '0.9'}s`
-    //   // elCentrale.style.transform = `translateX(-50%) ${String(this) === 'mousedown' ? 'scale(1.5)' : '' }`;
-    //   elCentrale.querySelector('.immagineInVetrina').style.transform = `${String(this) === 'mousedown' ? 'scale(1.5)' : '' }`}};
-
-    //   vetrinaEl.addEventListener(el, callback.bind(el))})
-
 }
-fnVetrina()
+fnVetrina(10)
+
+// ORDINA PER ORDINE ALFABETICO
+
+
+const funzioneSort = function (nonrev = true, nonNum = true) {
+
+  const markup = articoliArr.toSorted((a, b) => {
+    if(!nonNum) { return nonrev ? b.inserimento - a.inserimento : a.inserimento - b.inserimento} 
+    else{
+    if(!nonrev) {
+      if (a.titolo.trim().toUpperCase() > b.titolo.trim().toUpperCase()) return -1;
+      if (a.titolo.trim().toUpperCase() < b.titolo.trim().toUpperCase()) return 1
+    } else{
+      if (a.titolo.trim().toUpperCase() > b.titolo.trim().toUpperCase()) return 1;
+      if (a.titolo.trim().toUpperCase() < b.titolo.trim().toUpperCase()) return -1
+    }}}).map(el => `    
+      <div class="articoli container">
+        <figure class="figure-img">
+          <img class="img_art" src=${el.img} alt="">
+        </figure>
+        <div class="articoli_label">
+        <h4 class="nomearticolo">${el.titolo}</h4>
+        <p class="categoriaarticolo">${el.tipo}</p>
+        </div>
+      </div>`).join('')
+
+    document.querySelector('.contenitoreArticoli').innerHTML = '';
+    document.querySelector('.contenitoreArticoli').insertAdjacentHTML('afterbegin', markup)
+
+    upperLista()
+}
+
+
+document.getElementById('sortAl').addEventListener('click', funzioneSort)
+document.getElementById('sortRevAl').addEventListener('click', function() {
+  funzioneSort(false)
+})
+document.getElementById('sortNum').addEventListener('click', function () {
+  funzioneSort(true, false)
+})
+document.getElementById('sortRevNum').addEventListener('click', function () {
+  funzioneSort(false, false)
+})
+
+document.getElementById('mettiInVet').addEventListener('click', function (e) {
+  vetrinaEl.classList.add('hidden-1');
+  document.body.scrollIntoView({ behavior: 'smooth' })
+  new Promise (function (fulfill, reject) {
+    setTimeout(function () {
+      fulfill()
+    }, 800)
+  }).then(function () {
+    const markup = articoliArr.map(el => `<div class="articoloInVetrina transitionclass sfocArtVet"><img class="immagineInVetrina" src="${el.img}" alt="${el.titolo}"></div>`)
+    .join('') 
+    + articoliArr.map(el => `<div class="articoloInVetrina transitionclass sfocArtVet"><img class="immagineInVetrina" src="${el.img}" alt="${el.titolo}"></div>`)
+    .join('') 
+    + `<button class="btn_vet next-pg hidden-4"><i class="fa-solid fa-arrow-right"></i></button><button class="btn_vet prev-pg hidden-4"><i class="fa-solid fa-arrow-left"></i></button>`
+  vetrinaEl.innerHTML = '';
+  vetrinaEl.insertAdjacentHTML('afterbegin', markup)
+  pulsanti = [...document.querySelectorAll('.btn_vet')];
+  fnVetrina(articoliArr.length)
+  vetrinaEl.classList.remove('hidden-1')
+  }) 
+})
+
+
+
+
+
+// VETRINA
+
+
+
+
 
 // RICERCA
 document.getElementById('shopSubmit').addEventListener('click', function (e) {
   document.getElementById('shopRicerca').value = ''
 })
 
-// settare Uppercase lista
-const categorie = [...document.querySelectorAll('.categoriaarticolo')]
-categorie.forEach(c => c.textContent = c.textContent[0].toUpperCase() + c.textContent.slice(1))
 
 
-// Js-image-zoom
-var options = {
-  width: 350, 
-  height: 350,
-  zoomPosition: "original"
-};
-
-new ImageZoom(document.getElementById("img-container"), options);
 
 
 const mostraDetProdotto = function (data, e) {
-  console.log('PRIMA');
-  console.log(data);
-  const container = document.querySelector('#img-container')
-    container.innerHTML= ''
-    const img = `<img src="${e.target.closest('.articoli').querySelector('.img_art').src}" style="object-fit: contain;"/>`
-    container.insertAdjacentHTML('afterbegin', img)
-   new ImageZoom(document.getElementById("img-container"), options);
+  const container = document.querySelector('.magnify-wrapper');
+    container.innerHTML= '';
+    const img = `<img
+    src="${e.target.closest('.articoli').querySelector('.img_art').src}"
+    id="main-img" />
+    <div id="large-img" style = "background: url(${e.target.closest('.articoli').querySelector('.img_art').src}) no-repeat #fff"></div>`;
+    container.insertAdjacentHTML('afterbegin', img);
+    menuArticoliEl.scrollIntoView({ behavior: 'smooth' })
+
 
    const contenitoreTit = document.querySelector('.caratteristicheProd');
    contenitoreTit.innerHTML = '';
    const markupTit = `<h4 class="titoloArticolo">${e.target.closest('.articoli').querySelector('.nomearticolo').textContent}</h4>`
-   contenitoreTit.insertAdjacentHTML('afterbegin', markupTit)
+   contenitoreTit.insertAdjacentHTML('afterbegin', markupTit);
+  
+  
+
 }
 
 // eventlistener click lista
-const listaEl = document.querySelector('.shopsinistra')
 
 listaEl.addEventListener('click', function (e) {
   if(e.target.closest('.articoli')) {
@@ -233,3 +275,48 @@ document.querySelector('.shopsinistra').addEventListener('mouseleave' , function
   document.getElementById('hamMenuLista').classList.add('hidden-1')
 })
 
+
+
+
+
+
+// MAGNIFIER
+
+document.getElementById('zoom').addEventListener(
+  'mousemove',
+  function (e) {
+    let original = document.getElementById('main-img'),
+      magnified = document.getElementById('large-img'),
+      style = magnified.style,
+      x = e.pageX - this.offsetLeft,
+      y = e.pageY - this.offsetTop,
+      imgWidth = original.width,
+      imgHeight = original.height,
+      xperc = (x / imgWidth) * 200,
+      yperc = (y / imgHeight) * 200;
+      original.style.opacity = '0'
+    // Add some margin for right edge
+    if (x > 0.01 * imgWidth) {
+      xperc += 0.15 * xperc;
+    }
+
+    // Add some margin for bottom edge
+    if (y >= 0.01 * imgHeight) {
+      yperc += 0.15 * yperc;
+    }
+
+    // Set the background of the magnified image horizontal
+    style.backgroundPositionX = xperc - 40 + '%';
+    // Set the background of the magnified image vertical
+    style.backgroundPositionY = yperc - 40 + '%';
+
+    // Move the magnifying glass with the mouse movement.
+    style.left =  -100 + 'px';
+    style.top =  -100 + 'px';
+  },
+  false
+);
+
+document.getElementById('zoom').addEventListener(
+  'mouseout',
+  function (e) { e.target.style.opacity = '1'})
