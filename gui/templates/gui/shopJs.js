2d1c52fbccc09fb56c6ec07dde34c0e4d751f0c2
AngeloRan <arandazzo300990@gmail.com>
2024-01-02 12:55:44 +0100

    ingrandimento, array prodotti

--- gui/templates/gui/shopJs.js
+++ gui/templates/gui/shopJs.js
@@ -1,3 +1,5 @@
+
+
 {% load static %}
 
 config.rivelatoreSezioni()
@@ -5,29 +7,54 @@ config.rivelatoreSezioni()
 // settare Uppercase lista
 
 
-// console.log('{{articoli}}');
+// console.log('{{articoli}}'.json());
 
-const articoliArr = [...document.querySelectorAll('.articoli')].map(function (el) {
-  // console.log(el.dataset.Id);
+const createVetArr = function (list = [...document.querySelectorAll('.articoloInVetrina')]) {
+  return list.map(function (el, i, arr) {
   return {
-    titolo: el.querySelector('.nomearticolo').textContent,
-    tipo: el.querySelector('.categoriaarticolo').textContent,
+    titolo: el.querySelector('.immagineInVetrina').alt,
+    tipo: el.dataset.descrizioneBreve,
     inserimento: el.dataset.inserimento,
-    img: el.querySelector('.img_art').src,
-    id: el.dataset.id
+    img: el.querySelector('.immagineInVetrina').src,
+    id: +el.dataset.id,
+    descrizioneLunga: el.dataset.descrizione
   }
-})
+}).splice(list.length/2, list.length/2)
+};
 
-console.log(articoliArr);
+const createArtArr = function (list = [...document.querySelectorAll('.articoli')]) {
+  return list.map(function (el) {
+    // console.log(el.dataset.Id);
+    return {
+      titolo: el.querySelector('.nomearticolo').textContent,
+      tipo: el.querySelector('.categoriaarticolo').textContent,
+      inserimento: el.dataset.inserimento,
+      img: el.querySelector('.img_art').src,
+      id: +el.dataset.id,
+      descrizioneLunga: el.dataset.descrizione
+    }
+  })
+}
+
+const createArrTot = function (a = articoliArr, b = articoliInVetrinaArr) {
+ return [...a, ...b ]
+}
+
+const vetrinaIniziale = createVetArr();
+let articoliArr = createArtArr();
+let articoliInVetrinaArr = createVetArr();
+let articoliTotali = createArrTot();
+
+console.log('INIZIALE',vetrinaIniziale);
+console.log('VETRINA', articoliInVetrinaArr);
+console.log('ARTICOLI', articoliArr);
 
 
 
-// const articoliArr = [
 
-//   {titolo: 'naruto', tipo: 'statuina', numero: 4, img: '../../static/images/Grut.png', inserimento: Date.now() - 500,}, 
-//   {titolo: 'harry Potter', tipo: 'statuina', numero: 4, img: 'https://i.pinimg.com/564x/ab/51/f2/ab51f2dd6f1bbd2c8c435e4e8d403d93.jpg', inserimento: Date.now() - 1000}, {titolo: ' giangiukan', tipo: 'statuina', numero: 4, img: 'https://i.pinimg.com/564x/ab/51/f2/ab51f2dd6f1bbd2c8c435e4e8d403d93.jpg', inserimento: Date.now() - 2000}, {titolo: 'dragonball', tipo: 'statuina', numero: 4, img: 'https://i.pinimg.com/564x/ab/51/f2/ab51f2dd6f1bbd2c8c435e4e8d403d93.jpg', inserimento: Date.now() - 700}, {titolo: 'ilsignoredeglianelli', tipo: 'statuina', numero: 4, img: 'https://i.pinimg.com/564x/ab/51/f2/ab51f2dd6f1bbd2c8c435e4e8d403d93.jpg', inserimento: Date.now() - 8000}
+console.log('TOTALI', articoliTotali);
 
-// ];
+
 
 
 
@@ -173,7 +200,9 @@ const fnVetrina = function (totale) {
   } )
 
 }
-fnVetrina(10)
+
+console.log('NUMERO', articoliInVetrinaArr.length);
+fnVetrina(articoliInVetrinaArr.length)
 
 // ORDINA PER ORDINE ALFABETICO
 
@@ -228,15 +257,25 @@ document.getElementById('mettiInVet').addEventListener('click', function (e) {
       fulfill()
     }, 800)
   }).then(function () {
-    const markup = articoliArr.map(el => `<div class="articoloInVetrina transitionclass sfocArtVet"><img class="immagineInVetrina" src="${el.img}" alt="${el.titolo}"></div>`)
+    const markup = articoliArr.map(el => `<div class="articoloInVetrina transitionclass sfocArtVet" data-id="${el.id}" data-descrizione="${el.descrizioneLunga}" data-inserimento="${el.inserimento}" data-descrizioneBreve = "${el.tipo}">
+
+    <img class="immagineInVetrina" src="${el.img}" alt="${el.titolo}">
+  
+    </div>`)
     .join('') 
-    + articoliArr.map(el => `<div class="articoloInVetrina transitionclass sfocArtVet"><img class="immagineInVetrina" src="${el.img}" alt="${el.titolo}"></div>`)
+    + articoliArr.map(el => `<div class="articoloInVetrina transitionclass sfocArtVet" data-id="${el.id}" data-descrizione="${el.descrizioneLunga}" data-inserimento="${el.inserimento}" data-descrizioneBreve = "${el.tipo}">
+
+    <img class="immagineInVetrina" src="${el.img}" alt="${el.titolo}">
+  
+    </div>`)
     .join('') 
     + `<button class="btn_vet next-pg hidden-4"><i class="fa-solid fa-arrow-right"></i></button><button class="btn_vet prev-pg hidden-4"><i class="fa-solid fa-arrow-left"></i></button>`
   vetrinaEl.innerHTML = '';
   vetrinaEl.insertAdjacentHTML('afterbegin', markup)
+  articoliInVetrinaArr = createVetArr()
+  articoliTotali = createArrTot()
   pulsanti = [...document.querySelectorAll('.btn_vet')];
-  fnVetrina(articoliArr.length);
+  fnVetrina(articoliInVetrinaArr.length);
   vetrinaEl.classList.remove('hidden-1')
   }) 
 })
@@ -250,15 +289,26 @@ document.getElementById('shopSubmit').addEventListener('click', function (e) {
 
 
 
-
+console.log(articoliTotali.find(el=>el.id === 5));
 
 const mostraDetProdotto = function (e, vet = false) {
+
+  const cont = document.querySelector('.magnify-wrapper2');
+  const markup2 = ` <div id="lenteIngrandimento"><i class="fa-solid fa-magnifying-glass-plus"></i></div>
+  <img
+    src="${articoliTotali.find(el => el.id === (vet ? +e.target.closest('.articoloInVetrina').dataset.id : +e.target.closest('.articoli').dataset.id)).img}"
+    id="main-img2" />`;
+  cont.innerHTML = '';
+  cont.insertAdjacentHTML('afterbegin', markup2);
+
+
+
   const container = document.querySelector('.magnify-wrapper');
     container.innerHTML= '';
     const img = `<div id="lenteIngrandimento"><i class="fa-solid fa-magnifying-glass-plus"></i></div><img
-    src="${vet ? e.target.closest('.articoloInVetrina').querySelector('.immagineInVetrina').src : e.target.closest('.articoli').querySelector('.img_art').src}"
+    src="${articoliTotali.find(el => el.id === (vet ? +e.target.closest('.articoloInVetrina').dataset.id : +e.target.closest('.articoli').dataset.id)).img}"
     id="main-img" />
-    <div id="large-img" class="largeImg" style = "background: url(${vet ? e.target.closest('.articoloInVetrina').querySelector('.immagineInVetrina').src : e.target.closest('.articoli').querySelector('.img_art').src}) no-repeat #fff"></div>`;
+    <div id="large-img" class="largeImg" style = "background: url(${articoliTotali.find(el => el.id === (vet ? +e.target.closest('.articoloInVetrina').dataset.id : +e.target.closest('.articoli').dataset.id)).img}) no-repeat #fff"></div>`;
     container.insertAdjacentHTML('afterbegin', img);
     menuArticoliEl.scrollIntoView({ behavior: 'smooth' })
     document.getElementById('lenteIngrandimento').addEventListener('click', function (e) {
@@ -269,8 +319,15 @@ const mostraDetProdotto = function (e, vet = false) {
 
    const contenitoreTit = document.querySelector('.caratteristicheProd');
    contenitoreTit.innerHTML = '';
-   const markupTit = `<h4 class="titoloArticolo">${vet ? e.target.closest('.articoloInVetrina').querySelector('.immagineInVetrina').alt: e.target.closest('.articoli').querySelector('.nomearticolo').textContent}</h4>`
+   const markupTit = `<h4 class="titoloArticolo">${articoliTotali.find(el => el.id === (vet ? +e.target.closest('.articoloInVetrina').dataset.id : +e.target.closest('.articoli').dataset.id)).titolo}</h4>`
    contenitoreTit.insertAdjacentHTML('afterbegin', markupTit);
+
+   const containerRigaInf = document.querySelector('.dettagliorigaInf')
+   const markup3 = `        
+   <h4 class="titoloPiccolo">${articoliTotali.find(el => el.id === (vet ? +e.target.closest('.articoloInVetrina').dataset.id : +e.target.closest('.articoli').dataset.id)).titolo}</h4>
+   <p class="descrizioneLunga"><pre>${articoliTotali.find(el => el.id === (vet ? +e.target.closest('.articoloInVetrina').dataset.id : +e.target.closest('.articoli').dataset.id)).descrizioneLunga}</pre></p>`
+   containerRigaInf.innerHTML = '';
+   containerRigaInf.insertAdjacentHTML("afterbegin", markup3)
 
 }
 
@@ -321,9 +378,12 @@ const zoomImmagine = function (e) {
     y = e.pageY - this.offsetTop,
     imgWidth = original.width,
     imgHeight = original.height,
-    xperc = (x / imgWidth) * 200,
-    yperc = (y / imgHeight) * 150;
-    // original.style.opacity = '0'
+    // xperc = (x / imgWidth) * 200,
+    // yperc = (y / imgHeight) * 150;
+    xperc = (x / imgWidth) * 100,
+    yperc = (y / imgHeight) * 100;
+    original.style.opacity = '0'
+    magnified.style.opacity = '1'
   // Add some margin for right edge
   if (x > 0.01 * imgWidth) {
     xperc += 0.15 * xperc;
@@ -335,43 +395,44 @@ const zoomImmagine = function (e) {
   }
 
   // Set the background of the magnified image horizontal
-  style.backgroundPositionX = xperc - 40 + '%';
+  style.backgroundPositionX = xperc - 9 + '%';
   // Set the background of the magnified image vertical
-  style.backgroundPositionY = yperc - 40 + '%';
+  style.backgroundPositionY = yperc - 9 + '%';
 
   // Move the magnifying glass with the mouse movement.
-  style.left =  -100 + 'px';
-  style.top =  -100 + 'px';
+  style.left =  -0 + 'px';
+  style.top =  -0 + 'px';
 }
 
-document.getElementById('zoom').addEventListener('click', function (e) {
+
+document.getElementById('zoomDaZoom').addEventListener('click', function (e) {
  if(e.target.closest('#lenteIngrandimento')) {
-  // document.getElementById('large-img').classList.add('largeImg')
-  document.getElementById('large-img').style.opacity = '1'
+ 
+  if(!document.querySelector('.overlay').classList.contains('overlayClass')) {document.querySelector('.overlay').classList.add('overlayClass')}
+  document.querySelector('.overlay').scrollIntoView({behavior: 'instant'})
+  // document.querySelector('.magnify-wrapper').scrollIntoView({behavior: 'smooth'});
+  document.body.style.overflow = 'hidden'
  }
 })
 
 document.getElementById('zoom').addEventListener(
-  'mousemove', zoomImmagine,false);
+  'mousemove', zoomImmagine, false);
 
+ const chiudiOverlay = function () {
 
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && document.getElementById('large-img').classList.contains('largeImg')) {
-    // document.getElementById('large-img').classList.remove('largeImg');
-    document.getElementById('large-img').style.opacity = '0'
-  }
-});
+ document.addEventListener('keydown', function (e) {
+    if(e.key === 'Escape' && document.querySelector('.overlay').classList.contains('overlayClass')) {document.querySelector('.overlay').classList.remove('overlayClass')};
+    document.body.style.overflow = 'auto';
+ });
 
-document.addEventListener('click', function (e) {
-  if(!e.target.closest('#zoom') && document.getElementById('large-img').classList.contains('largeImg')) {
-    // document.getElementById('large-img').classList.remove('largeImg');
-    document.getElementById('large-img').style.opacity = '0'
-  }
-})
+  document.querySelector('.overlay').addEventListener('click', function (e) {
+    if(!e.target.closest('.magnify-wrapper') && document.querySelector('.overlay').classList.contains('overlayClass')) {
+      document.querySelector('.overlay').classList.remove('overlayClass');
+      document.body.style.overflow = 'auto';
+      }
+  })
+}
 
-// document.getElementById('zoom').addEventListener(
-//   'mouseleave',
-//   function (e) { 
-//   document.getElementById('large-img').classList.remove('largeImg')
-//   })
+chiudiOverlay()
+
 
