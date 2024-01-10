a4499f4357e3b9ba0718c448cbaff9eb8a21b87a
AngeloRan <arandazzo300990@gmail.com>
2024-01-10 20:36:14 +0100

    query

--- gui/views.py
+++ gui/views.py
@@ -33,7 +33,7 @@ def shop(request):
     if request.method == 'GET':
 
         #@TODO set it in settings
-        pagination_count = 5
+        pagination_count = 1
 
         data_only = request.GET.get('data_only', False)
         page = request.GET.get('page', 1)
